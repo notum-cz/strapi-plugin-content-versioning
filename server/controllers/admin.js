c7c734c6548a50fb08800d7cf0d08add0c5f2be7
Filip Janko <fikoun@icloud.com>
2023-01-23 18:19:51 +0100

    added cloning (+patch)

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { v4: uuid } = require("uuid");
 const { getService } = require("../utils");
 
 module.exports = {
@@ -9,6 +10,31 @@ module.exports = {
     const { user } = ctx.state;
 
     const { createVersion } = getService("core-api");
+
+    // Clone version tree
+    if (data.isDuplicatingEntry) {
+      const allVersions = await strapi.db.query(slug).findMany({
+        where: {
+          vuid: data.vuid,
+          //locale: data.locale // Clone all or only this locale ?
+        },
+        sort: [{ versionNumber: 'asc' }], // Incrementaly create versions
+      });
+  
+      let initialCloneVersion = null;
+      const newVuid = uuid();
+  
+      for (const versionData of allVersions) {
+        versionData.vuid = newVuid;
+        const created = await createVersion(slug, versionData, user, {});
+        
+        // identify first clone version to be returned
+        if (created.locale == data.locale && created.versionNumber == data.versionNumber) {
+          initialCloneVersion = created
+        }
+      }
+      return initialCloneVersion;
+    }
 
     return await createVersion(slug, data, user, ctx.request.query);
   },
