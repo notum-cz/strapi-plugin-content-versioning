110c16e15b9b99e1ab4982f82ab7191404b2b9f2
aplavsa <22588290+aplavsa@users.noreply.github.com>
2023-02-16 12:47:50 +0100

    code refactoring

--- server/middlewares/relationUpdate.js
+++ server/middlewares/relationUpdate.js
@@ -14,104 +14,92 @@ const relationUpdateMiddleware = async (ctx, next) => {
     const { model, id } = ctx.request.params
     const modelDef = strapi.getModel(model);
     if (!getService("content-types").isVersionedContentType(modelDef)) {
-        console.log("It is not versioned content type");
         return next();
     }
     // console.log(strapi.contentTypes); 
     const entry = await strapi.entityService.findOne(model, id);
-    console.log(entry);
-    const allVersionIds = await strapi.entityService.findMany(model, { fields: ["id"], filters: { vuid: entry.vuid } });
-    console.log(allVersionIds);
-    if (allVersionIds.length < 2) {
+    const allVersionIds = await strapi.entityService.findMany(model, { fields: ["id"], filters: { vuid: entry.vuid } })
+    const allVersionIdsNumbers = allVersionIds.map(id => id.id);
+    if (allVersionIdsNumbers.length < 2) {
         // there are no multiple version, no need to update relations
         return next();
     }
-    const allVersionIdsNumbers = allVersionIds.map(id => id.id);
     //find all content types that might have relation to this content type
-    // Object.entries(strapi.contentTypes).forEach(([key, item]) => console.log(item.attributes)); 
     const contentTypes = Object.entries(strapi.contentTypes);
     const components = strapi.components;
+    const matchedComponents = Object.entries(components).map(mappingFn(model)).flat();
+    const matchedContentTypes = contentTypes.map(mappingFn(model, ({ name }) => name !== "localizations" && name !== "versions")).flat();
+
+    const allLinkedComponents = [matchedComponents, matchedContentTypes].flat();
+    //find all relations that point to one of available ids
+    allLinkedComponents.forEach(findAndUpdateRelations(allVersionIdsNumbers, id))
+    return next();
+}
+
+function findAndUpdateRelations(allVersionIdsNumbers, id) {
+    return async (component) => {
+        const populateQuery = {};
+        const filtersQuery = {};
+        component.attributes.forEach(attr => {
+            populateQuery[attr.name] = {
+                filters: {
+                    id: allVersionIdsNumbers
+                }
+            };
+            filtersQuery[attr.name] =
+                allVersionIdsNumbers;
+        });
+        const results = await strapi.entityService.findMany(component.key, {
+            populate: populateQuery,
+            filters: filtersQuery
+        });
+        //update all content types to the latest published version
+        results.forEach(async (result) => {
+            await strapi.entityService.update(component.key, result.id, {
+                data: {
+                    ...generateUpdateData(result, component.attributes, id, allVersionIdsNumbers)
+                }
+            });
+        });
+    };
+}
+
+function mappingFn(model, additionalPredicate) {
     const predicate = _.matchesProperty("type", "relation")
     const targetPredicate = _.matchesProperty("target", model)
-    const matchedComponents = Object.entries(components).map(([key, item]) => {
-        const result = Object.entries(item.attributes).filter(([aKey, value]) => predicate(value) && targetPredicate(value)).map(([akey, value]) => ({ name: akey, relationType: value.relation }));
+    return ([key, item]) => {
+        let result = Object.entries(item.attributes).filter(([aKey, value]) => predicate(value) && targetPredicate(value)).map(([akey, value]) => ({ name: akey, relationType: value.relation }));
+        if (typeof additionalPredicate === "function") {
+            result = result.filter(additionalPredicate)
+        }
         if (result.length === 0) {
             return [];
         } else {
             return {
                 key: key,
                 attributes: result
-            }
+            };
         }
-    }).flat();
-    console.log("matched components", matchedComponents)
-    const matchedContentTypes = contentTypes.map(([key, item]) => {
-
-        const result = Object.entries(item.attributes).filter((([aKey, val]) => predicate(val) && targetPredicate(val) && aKey !== "localizations" && aKey !== "versions")).map(([akey, val]) => { return { name: akey, relationType: val.relation } })
-        if (result.length === 0) {
-            return []
-        } else {
-            return {
-                key: key,
-                attributes: result
-            }
-        }
-
-    }).flat();
-
-    const allLinkedComponents = [matchedComponents, matchedContentTypes].flat();
-    console.log("matched content types", matchedContentTypes)
-    //find all relations that point to one of available ids
-    allLinkedComponents.forEach(async component => {
-        const populateQuery = {};
-        component.attributes.forEach(attr => {
-            populateQuery[attr.name]= {
-                filters: {
-                    id: allVersionIdsNumbers
-                }
-            }
-        })
-        const filtersQuery = {};
-        component.attributes.forEach(attr=> {
-            filtersQuery[attr.name]= 
-                allVersionIdsNumbers
-            
-        })
-        const results = await strapi.entityService.findMany(component.key, {
-            populate: populateQuery,
-            filters: filtersQuery
-        })
-        console.log("results"+component.key, results); 
-        results.forEach(async result => {
-            await strapi.entityService.update(component.key, result.id, {data: {
-                ...generateUpdateData(result, component.attributes, id, allVersionIdsNumbers)
-            }})
-        })
-    })
-
-    //update all content types to the latest published version
-
-    strapi.log.http("Relation update middleware")
-    return next();
+    };
 }
 
 function generateUpdateData(result, attributes, id, allIds) {
-    const updateData = {}; 
+    const updateData = {};
     attributes.forEach(attr => {
-        let resultAttribute = result[attr.name]; 
-        if(typeof resultAttribute === "object"){
+        let resultAttribute = result[attr.name];
+        if (typeof resultAttribute === "object") {
             updateData[attr.name] = {
                 disconnect: [resultAttribute.id],
                 connect: [id]
             }
-        } else if(Array.isArray(resultAttribute)){
+        } else if (Array.isArray(resultAttribute)) {
             updateData[attr.name] = {
                 disconnect: resultAttribute.map(resu => resu.id).filter(resultId => resultId !== id).filter(resultId => allIds.includes(resultId)),
                 connect: [id]
             }
         }
     })
-    return updateData; 
+    return updateData;
 }
 
 module.exports = {
