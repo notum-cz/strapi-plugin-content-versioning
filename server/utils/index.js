a5e3c9c96689544bf14344afc250f7491e6bd947
Filip Janko <fikoun@icloud.com>
2023-02-22 18:57:13 +0100

    fixed advanced queries to support MySQL and SQLite

--- server/utils/index.js
+++ server/utils/index.js
@@ -11,37 +11,35 @@ const getService = name => {
   return strapi.plugin(pluginId).service(name);
 };
 
-// TODO: ...?
 const isLocalizedContentType = (model) => {
   return strapi.plugin('i18n')?.service('content-types')?.isLocalizedContentType(model);
 };
 
-const getLatestRawQuery = (model, vuid, excludeId) => {
-  const cols = ['id', 'version_number', 'published_at'];
-  const orderBy = ['published_at'];
-  const distinct = [];
-  const where = [];
+const getLatestRawQuery = (model, vuid) => {
+  let rawQuery;
 
   if (isLocalizedContentType(model)) {
-    cols.push('locale')
-    orderBy.unshift('locale')
-    distinct.unshift('locale')
+    rawQuery = `SELECT a.id, a.locale, a.version_number, a.published_at
+      FROM ${model.collectionName} a WHERE NOT EXISTS (
+      SELECT 1 FROM ${model.collectionName} WHERE locale=a.locale AND vuid=a.vuid AND`
+  } else {
+    rawQuery = `SELECT a.id, a.version_number, a.published_at
+      FROM ${model.collectionName} a WHERE NOT EXISTS (
+      SELECT 1 FROM ${model.collectionName} WHERE vuid=a.vuid AND`
   }
+
+  rawQuery += ` (
+    CASE WHEN a.published_at is null THEN (
+      published_at is not null OR version_number > a.version_number
+   )
+   ELSE published_at is not null AND version_number > a.version_number
+   END
+   )
+ )`
 
   if (vuid) {
-    where.push(`vuid='${vuid}'`)
-  } else {
-    orderBy.unshift('vuid')
-    distinct.unshift('vuid')
+    rawQuery += ` AND vuid = '${vuid}'`;
   }
-  if (excludeId) {
-    where.push(`id!='${excludeId}'`)
-  }
-
-  let rawQuery = distinct.length ? `SELECT DISTINCT ON (${distinct.join(',')}) ` : `SELECT `
-  rawQuery += cols.join(', ') + ` FROM ${model.collectionName} `
-  rawQuery += (where.length ? `WHERE ${where.join(' AND ')}` : '') + ` ORDER BY ${orderBy.join(', ')} `
-  rawQuery += 'DESC NULLS LAST, version_number DESC'
 
   return rawQuery;
 };
