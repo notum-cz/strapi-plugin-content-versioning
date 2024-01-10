20609f348f3f313b0190a2008d145560baf19bc9
Ondrej Mikulcik <ondrej.mikulcik@notum.cz>
2024-01-10 11:46:05 +0100

    fix: many to many disconnections

--- server/middlewares/relationUpdate.js
+++ server/middlewares/relationUpdate.js
@@ -143,7 +143,8 @@ function generateUpdateData(result, attributes, id, allIds) {
         .filter((resultId) => resultId !== id)
         .filter((resultId) => allIds.includes(resultId));
       updateData[attr.name] = {
-        disconnect: otherVersionsOfEntry,
+        disconnect:
+          attr.relationType === "manyToMany" ? [] : otherVersionsOfEntry,
         connect: otherVersionsOfEntry.length ? [id] : [],
       };
     } else if (resultAttribute && typeof resultAttribute === "object") {
@@ -157,6 +158,7 @@ function generateUpdateData(result, attributes, id, allIds) {
       };
     }
   });
+  console.log(updateData);
   return updateData;
 }
 
