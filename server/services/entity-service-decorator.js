2d238296753129f26866b42e8d44de96495a16d9
Ondrej Mikulcik <ondrej.mikulcik@notum.cz>
2024-03-19 11:04:48 +0100

    fix: relations connections

--- server/services/entity-service-decorator.js
+++ server/services/entity-service-decorator.js
@@ -284,9 +284,10 @@ const decorator = (service) => ({
       // omit current locale
       data.localizations = Object.values(_.omit(latestByLocale, data.locale));
     }
+    console.log("gets here");
     // remove old ids
     const newData = await createNewVersion(uid, data, model);
-
+    console.log("gets here 2", newData);
     // Create Version
     const result = await service.create.call(this, uid, {
       ...opts,
@@ -295,6 +296,7 @@ const decorator = (service) => ({
         publishedAt: null,
       },
     });
+    console.log("gets here 3");
     // Relink all versions from other locales if result is The latest(published)!
     if (result.isVisibleInListView && isLocalized) {
       // !set the current as latest in locale
